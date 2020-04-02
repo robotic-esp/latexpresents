9e7d6bd3079ad443954bac255a4f779a8205f97d
Jonathan Gammell <gammell@gmail.com>
2020-04-02 08:57:55 +0100

    Better and more safe thumbnail and encoding commands.

--- cmds/encode_videos.sh
+++ cmds/encode_videos.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-INPUT="${1}"
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of parameters"
+    exit 2
+fi
 
-ls "${INPUT}"
+INPUT="${1%/*}"
 
 # -g is the key-frame rate, the lower the number, the more keyframes. (e.g., -g X creates a keyframe every X frames).
 
+# Make a src folder, regardless of whether it already exists
+mkdir -p "${INPUT}/src"
+
 for vid in "${INPUT}"/*; do
-    if [ "${vid: -3}" != "png" ] && [ "${vid: -3}" != "jpg" ] && [ "${vid: -3}" != "gif" ] && [ "${vid: -3}" != "pdf" ]; then
-        filename="${vid%.*}"
-        if [ "${filename: -4}" != "_enc" ]; then
+    if [[ -f ${vid} && "${vid: -3}" != "png" && "${vid: -3}" != "jpg" && "${vid: -3}" != "gif" && "${vid: -3}" != "pdf" ]]; then
+        srcvid="${INPUT}/src/${vid#${INPUT}/}"
+        filename="${vid%.*}_enc.mp4"
+        trgtvid="${INPUT}/${filename#${INPUT}/}"
+
+        # A video can only be reencoded iff (i) its name does not suggest it was previously reencoded (ii) it can safely be copied to src/ (iii) the target reencoded file name is available
+        if [[ "${vid: -8}" != "_enc.mp4" && ! -f "${srcvid}" && ! -f "${trgtvid}" ]]; then
+            echo "${vid}:"
+            echo "    Moving ${vid} to ${srcvid}."
+            mv -n "${vid}" "${srcvid}"
+
+            echo "    Encoding ${srcvid} to ${trgtvid}."
             # Get the frame rate and evaluate to a real number.
-            fps=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=avg_frame_rate "${vid}")
+            fps=$(ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=avg_frame_rate "${srcvid}")
             # Encode with keyframes at a multiple of it
-            ffmpeg -n -i "${vid}" -c:a copy -c:v libx264 -preset veryslow -g "${fps}" "${filename}_enc.mp4" 
+            # Preserving audio            
+            ffmpeg -n -hide_banner -loglevel error -i "${srcvid}" -c:a copy -c:v libx264 -preset veryslow -g "${fps}" "${trgtvid}" 
+            # Removing audio
+            # ffmpeg -n -nostats -i "${vid}" -an -c:v libx264 -preset veryslow -g "${fps}" "${trgtvid}"
+        elif [[ "${vid: -8}" == "_enc.mp4" ]]; then
+            echo "${vid}: Video appears to already be reencoded, will not proceed."
+        elif [[ -f "${srcvid}" ]]; then
+            echo "${vid}: Backup copy ${srcvid} already exists, cannot proceed."
+        elif [[ -f "${trgtvid}" ]]; then
+            echo "${vid}: Target ${trgtvid} already exists, cannot proceed."
         fi
     fi
 done
--- cmds/thumbnail_videos.sh
+++ cmds/thumbnail_videos.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-INPUT="${1}"
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of parameters"
+    exit 2
+fi
 
-ls "${INPUT}"
+INPUT="${1%/*}"
 
 for vid in "${INPUT}"/*; do
-    if [ "${vid: -3}" != "png" ] && [ "${vid: -3}" != "jpg" ] && [ "${vid: -3}" != "gif" ] && [ "${vid: -3}" != "pdf" ]; then
-        ffmpeg -n -i "${vid}" -vframes 1 "${vid%.*}.png"
+    if [[ -f ${vid} && "${vid: -3}" != "png" && "${vid: -3}" != "jpg" && "${vid: -3}" != "gif" && "${vid: -3}" != "pdf" ]]; then
+        thumb="${vid%.*}.png"
+        if [ ! -f "${thumb}" ]; then
+            echo "${vid}: Thumbnailing to ${thumb}."
+            ffmpeg -n -hide_banner -loglevel error -i "${vid}" -vframes 1 "${thumb}"
+        else
+            echo "${vid}: Thumbnail ${thumb} already exists."
+        fi
     fi
 done
