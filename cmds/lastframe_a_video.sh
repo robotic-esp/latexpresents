472800aa048a35d19e13357429be5f90ea4f2a25
Jonathan Gammell <gammell@gmail.com>
2021-02-04 19:57:10 +0000

    Command to create a thumbnail for the end of a video.

---
+++ cmds/lastframe_a_video.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of parameters"
+    exit 2
+fi
+
+VID="${1}"
+
+echo ${VID}
+if [[ -f ${VID} && "${VID: -3}" != "png" && "${VID: -3}" != "jpg" && "${VID: -3}" != "gif" && "${VID: -3}" != "pdf" ]]; then
+    last="${VID%.*}_last.png"
+    echo ${last}
+    if [ ! -f "${last}" ]; then
+        echo "${VID}: Last framing to ${last}."
+        ffmpeg -n -hide_banner -loglevel error -sseof -1 -i "${VID}"  -update 1 "${last}"
+    else
+        echo "${VID}: Last frame ${last} already exists."
+    fi
+fi
