04ffecfc79f02f8d5c829df813d2c42d2387593e
Jonathan Gammell <gammell@gmail.com>
2022-04-14 18:33:31 +0100

    Remove debug echos

--- cmds/lastframe_a_video.sh
+++ cmds/lastframe_a_video.sh
@@ -7,10 +7,8 @@ fi
 
 VID="${1}"
 
-echo ${VID}
 if [[ -f ${VID} && "${VID: -3}" != "png" && "${VID: -3}" != "jpg" && "${VID: -3}" != "gif" && "${VID: -3}" != "pdf" ]]; then
     last="${VID%.*}_last.png"
-    echo ${last}
     if [ ! -f "${last}" ]; then
         echo "${VID}: Last framing to ${last}."
         ffmpeg -n -hide_banner -loglevel error -sseof -1 -i "${VID}"  -update 1 "${last}"
